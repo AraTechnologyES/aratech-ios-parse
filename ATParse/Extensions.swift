73005a82bff8d38e9dbdf70f7ab3ef397cd33ba8
machukas <machukkas@gmail.com>
2018-05-07 16:38:50 +0200

    Extensión de PFObject en lugar de clase ATParseObject

---
+++ ATParse/Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  ATParseObject.swift
+//  ATParse
+//
+//  Created by Nicolas Landa on 4/1/17.
+//  Copyright © 2017 Nicolas Landa. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+public extension PFObject {
+	
+	/// Compara dos objectos ATParse
+	///
+	/// - Parameters:
+	///   - lhs:
+	///   - rhs:
+	/// - Returns: Devuelve `true` si los `objectId` de ambos objetos coinciden
+	public static func == (lhs: PFObject, rhs: PFObject) -> Bool {
+		return lhs.objectId == rhs.objectId
+	}
+	
+	/// Recupera una propiedad de tipo `T` del PFObject
+	///
+	/// - Parameter key: La clave del valor a recuperar
+	/// - Returns: El valor de la propiedad recuperada
+	public func property<T>(forKey key: String) -> T? {
+		let value = self.object(forKey: key) as? T
+		return value
+	}
+	
+	/// Establece una propiedad de tipo `T` del PFObject
+	///
+	/// - Parameter key: La clave del valor a establecer
+	/// - Returns: El valor de la propiedad establecida
+	public func setProperty<T>(_ property: T, forKey key: String) {
+		self.setObject(property, forKey: key)
+	}
+}
